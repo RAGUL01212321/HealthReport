196e7ccbcf70815d4356c5cc273ca6d3b698ae54
RAGUL01212321 <uragul500@gmail.com>
2025-08-18 18:00:00 +0530

    resolved

--- Backend/models/Text_Extraction/tesseract.py
+++ Backend/models/Text_Extraction/tesseract.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import cv2
 import pytesseract
 import json
@@ -22,29 +21,4 @@ data={
 with open(json_path, 'w') as json_file:
     json.dump(data,json_file,indent=4)
 
-=======
-import cv2
-import pytesseract
-import json
-
-json_path="Data\Mid_Process\extracted_text.json"
-img_path = "Data/sample/image.png"
-
-img = cv2.imread(img_path)
-# Convert image to grayscale
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# Apply threshold to convert to binary image
-threshold_img = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-# Pass the image through pytesseract
-text = pytesseract.image_to_string(threshold_img)
-
-data={
-    "filename" : img_path,
-    "extracted_text" : text
-}
-
-with open(json_path, 'w') as json_file:
-    json.dump(data,json_file,indent=4)
-
->>>>>>> 71b1305c3e963b520cc8ffb681fe6adc0228cee5
 print("Extracted text")
